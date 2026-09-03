5ebfff3f309ceed22eaaa304e96ce4116b521236
Christopher BM <bamc1715@gmail.com>
2026-09-03 14:06:15 -0600

    html y css listos

---
+++ app.js
@@ -0,0 +1,21 @@
+/** 
+ * Todo 10 que seleccionemos al principio
+ * Debe ser seleccionado mediante el document
+ * 
+ * Opciones de seleccion
+ * Clasicas
+ * getElementById
+ * getElementByClassName
+ * 
+ * Modernas
+ * Nos permiten seleccionar por un selector css
+ * selector css
+ * etiqueta por ejemplo form
+ * clae . por ejemplo .form-control
+ * id # por ejepmlo #title
+ * 
+ * getQuerySelector()  //si usamos un selector como de clase solo va a seleccionar la primera coincidencia
+ * querySelector
+ * 
+ * 
+*/
